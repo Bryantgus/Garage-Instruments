1e0295bc78043464f476242d2154f9cfd90eacad
Bryant Tejeda <bryanttejeda628f@hotmail.com>
2025-01-25 20:39:54 -0400

    Clean branch

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import Piano from "./components/Piano"
+
+export default function App() {
+  
+  return (
+    <main>
+      <header><h1>Garage Instruments</h1></header>
+      <h2>Piano</h2>
+      <Piano />    
+    </main>
+  )
+}
